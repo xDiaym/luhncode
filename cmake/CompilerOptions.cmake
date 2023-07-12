d97709eb235587469a03af1c56f7a5a9c95eafca
Matthew Nekirov <matthew.nekirov@gmail.com>
2023-07-12 14:52:40 +0300

    build: refactor cmake

---
+++ cmake/CompilerOptions.cmake
@@ -0,0 +1,15 @@
+if (NOT DEFINED CMAKE_CXX_STANDARD)
+    set(CMAKE_CXX_STANDARD 20)
+endif()
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
+
+add_compile_options("-pipe" "-g" "-Wall" "-Wextra" "-Wpedantic")
+if (NOT LUHNCODE_NO_SIMD)
+    add_compile_options("-mavx2")
+endif ()
+
+set(CMAKE_CXX_FLAGS_RELEASE "-O2")
+if (CMAKE_BUILD_TYPE MATCHES "Release")
+    add_definitions("-DNDEBUG")
+endif()
